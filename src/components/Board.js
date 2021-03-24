24fff02c224d41d612af8b49d018b63026d8b839
Ikhsan Fauji <ikhsanfauji.dev@gmail.com>
2021-03-24 22:41:28 +0700

    squares prop

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,8 +2,15 @@ import React, { Component } from 'react';
 import Square from './Square';
 
 class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null)
+    };
+  }
+
   renderSquare(i) {
-    return <Square value={i} />
+    return <Square value={this.state.squares[i]} />
   }
 
   render() {
