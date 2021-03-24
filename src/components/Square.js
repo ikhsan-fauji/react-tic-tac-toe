718774269717c02ee4c7aab19b0d46f6744f583c
Ikhsan Fauji <ikhsanfauji.dev@gmail.com>
2021-03-24 21:36:21 +0700

    props initialization

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class Square extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
+
   render() {
     return (
       <button className="square" onClick={() => alert('On Click')}>
